d8318f795802cbf037d0f96d740ee2642ced8f5b
venkatreddy123 <venkatreddy7332@gmail.com>
2023-03-15 00:13:04 +0530

    Navigation

---
+++ core/src/main/java/com/adobe/aem/crea2minds/core/models/NavigationModelImpl.java
@@ -0,0 +1,21 @@
+package com.adobe.aem.crea2minds.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class NavigationModelImpl {
+
+    @ValueMapValue
+    private String navigationRoot;
+
+
+    public String getNavigationRoot() {
+        return navigationRoot;
+    }
+
+
+
+}
